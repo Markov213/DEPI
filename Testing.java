5ad162c78d4c1176b8d3d1d0f30f374435ce3269
Marwan Ehab Adel Mostafa <MarwanEhab754@gmail.com>
2025-11-01 20:04:52 +0000

    Add Testing 2.1

--- Testing.java
+++ Testing.java
@@ -1,3 +1,5 @@
-public static void main(String[] args){
-    System.out,println("Hello World");
+public static main{
+    public static void main(String[] args){
+        System.out,println("Hello World");
+    }
 }
